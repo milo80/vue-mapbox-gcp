f27bc470a35823008eba6eac2d6b637fe7560dfb
milo80 <miloaldevaran@yahoo.com.mx>
2023-08-07 11:47:31 -0600

    1st commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <MapComponent />
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+<script setup lang="ts">
+import MapComponent from "@/components/MapComponent.vue";
 </script>
 
 <style>
@@ -25,3 +18,4 @@ export default defineComponent({
   margin-top: 60px;
 }
 </style>
+
---
+++ src/components/MapComponent.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div id="map"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue';
+import mapboxgl from 'mapbox-gl';
+
+
+export default defineComponent({
+  name: 'MapComponent',
+  setup() {
+    onMounted(() => {
+      mapboxgl.accessToken = 'pk.eyJ1IjoibWlsb2FsZGV2YXJhbiIsImEiOiJjbGt5ZThuZzUxa3RxM3NtYXZqMTZ6Y3BlIn0.cy_ytb88EIHgGBXyofi2zQ';
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-74.5, 40],
+        zoom: 9,
+      });
+    });
+  },
+});
+</script>
